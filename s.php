1bdbaa58af47215eb1a78b1df8b79933b796b2cb
fancy-wong <814767658@qq.com>
2018-12-19 11:53:58 +0800

    second

--- s.php
+++ s.php
@@ -1,60 +1,6 @@
 <?php
 echo(strval(microtime(true)*10000));exit;
-		$servername = "localhost";
-$username = "cs_i_js_cn";
-$password = "6zXDb54NKDAa8dGs";
-$dbname = "cs_i_js_cn";
-$time = time();
-	$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("连接失败: " . $conn->connect_error);
-}
-	if(isset($_FILES["file1"])){
-		$arr1 = $_FILES["file1"];
-	$aaa1 = $_POST['url1'];
-	$filename1 = "demo/" .$arr1["name"];
-	move_uploaded_file($arr1["tmp_name"],$filename1);
-	$sql = "UPDATE ysk_config SET value = $filename1 WHERE name = tupian1";
-mysqli_query($conn, $sql);
-$sql = "UPDATE ysk_config SET value = $aaa1  WHERE name = lianjie1";
-mysqli_query($conn, $sql);
-	echo "上传成功";
-	echo '<script>alert("修改成功");</script>';
-	
-	}
-	
-	if(isset($_FILES["file2"])){
-		$arr2 = $_FILES["file2"];
-	$aaa2 = $_POST['url2'];
-	
-	$filename2 = "demo/" .$arr2["name"];
-	move_uploaded_file($arr2["tmp_name"],$filename2);
-	
-	$sql = "UPDATE ysk_config SET value = $filename1 WHERE name = tupian1";
-mysqli_query($conn, $sql);
-$sql = "UPDATE ysk_config SET value = $aaa1  WHERE name = lianjie1";
-mysqli_query($conn, $sql);
-	echo "上传成功";
-	echo '<script>alert("修改成功");</script>';
-	}
-	if(isset($_FILES["file3"])){
-		$arr3 = $_FILES["file3"];
-	$aaa3 = $_POST['url3'];
-	$filename3 = "demo/" .$arr3["name"];
-	move_uploaded_file($arr3["tmp_name"],$filename3);
-
-	$sql = "UPDATE ysk_config SET value = $filename1 WHERE name = tupian1";
-mysqli_query($conn, $sql);
-$sql = "UPDATE ysk_config SET value = $aaa1  WHERE name = lianjie1";
-mysqli_query($conn, $sql);
-	echo "上传成功";
-	echo '<script>alert("修改成功");</script>';
-	}
-
-
-
-
+		
 
 ?>
 <html xmlns="http://www.w3.org/1999/xhtml">
